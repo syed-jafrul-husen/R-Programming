baddb982b0b3f0b3ba546032fda7aa1c11807868
Syed Jafrul Husen <syedjafrul4@gmail.com>
2021-10-28 03:00:19 +0600

    basic package code added

---
+++ Lecture-05.R
@@ -0,0 +1,14 @@
+# package installation
+install.packages("gapminder")
+library(gapminder)
+# package should install one time but before use every time the library should be call
+
+# explore gapminder
+head(gapminder)
+tail(gapminder)
+dim(gapminder)
+str(gapminder)
+
+
+#documentation
+?gapminder
