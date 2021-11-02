79f1dcc08d611bdbe93570d4cfdb3cca4feef4fd
Syed Jafrul Husen <syedjafrul4@gmail.com>
2021-11-02 22:22:58 +0600

    dplyr-II use of pipe(), mutate() and arrange()

---
+++ Basic - 1/Lecture-08.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(gapminder)
+
+#pipe()
+sum(3,2) %>% mean() %>% log()
+
+#
+gapminder %>% head() 
+gapminder %>% 
+  select(country, pop) %>% 
+  filter(country=="Bangladesh")
+
+#Use of mutate() function to create a new column
+gapminder %>%
+  mutate(TotalGDP=pop*gdpPercap)
+
+#use of arrange() function---- sort
+gapminder %>%
+  mutate(TotalGDP=pop*gdpPercap) %>%
+  arrange(TotalGDP)
+
+
