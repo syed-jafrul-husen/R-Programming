a98125e6e03971bc2a6c0d195c4a0cfc1f843da6
Syed Jafrul Husen <syedjafrul4@gmail.com>
2021-11-05 10:08:36 +0600

    heatmap() code added

---
+++ Basic - 1/Lecture-12.R
@@ -0,0 +1,36 @@
+#install pheatmap
+
+install.packages("pheatmap")
+library(pheatmap)
+
+?pheatmap
+
+#make a dataframe/matrix
+head(mtcars)
+class(mtcars)
+
+df = scale(mtcars)
+
+pheatmap(df, 
+         border_color = "red",
+         cellwidth = 15,
+         cluster_rows = T,
+         cutree_rows = 3,
+         show_rownames = T,
+         fontsize = 7,
+         display_numbers = T)
+
+#
+pheatmap(mtcars)
+
+
+if(FALSE){
+  "We use scaling when the difference between 
+  dataframe value is higher and we can't 
+  see anything if we display the heapmat pattern"
+  
+  "Scalling function column and row er value 
+  gula ke kasakachi distribution e niye chole ashe"
+}
+
+  
