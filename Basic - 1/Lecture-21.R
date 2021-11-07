07aef89f3b6273dcbea013b40b7af047e236b3c3
Syed Jafrul Husen <syedjafrul4@gmail.com>
2021-11-07 12:04:50 +0600

    for loop code added

--- Basic - 1/Lecture-21.R
+++ Basic - 1/Lecture-21.R
@@ -1,1 +1,22 @@
-# Loop
+#Use of for loop print 1 to 10
+
+for(index in 1:10){
+  print(index)
+}
+
+for(index in 1:10){
+  print(c("index = ",index))  # we have to write c so that it can concrete the string
+}
+
+
+
+#multiply 3 by 1:10
+for(index in 1:10){
+  print(3*index)
+}
+
+#
+for(index in 1:10){
+  print(c(3, "*", index, "=", 3*index))
+}
+
