ac2de2d96e6e66cd344e20116f7850be4e6723d5
Syed Jafrul Husen <syedjafrul4@gmail.com>
2021-11-07 12:54:24 +0600

    Leukemia_Carcinoma data added for hiererchical clustering

---
+++ Basic - 1/Lecture-23.R
@@ -0,0 +1,17 @@
+if(F)
+{
+  
+"  Hierarchical clustering is and algorithm that groups
+  similar objects into groups called clustering. "
+  
+"  There has two hierarchical clustering approach:
+    1. bottom-up approach(based on similarity)
+    2. top-down approach(based on dissimilarity) "
+  
+  
+
+}
+
+
+install.packages("dendextend")
+library(dendextend)
