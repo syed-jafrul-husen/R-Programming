9ee07317226538523f66309a81cf7df51d619823
Syed Jafrul Husen <syedjafrul4@gmail.com>
2021-11-07 11:57:31 +0600

    if/else code added

---
+++ Basic - 1/Lecture-20.R
@@ -0,0 +1,27 @@
+#if statement
+
+x = 5
+
+if(x>0){
+  print("x is a positive number")
+}
+
+#Else statement
+x = -5
+if(x>0){
+  print("X is a positive number")
+} else{
+  print("X is a negative number")
+}
+
+
+# add else if
+x = 0
+if(x>0){
+  print("X is a positive number")
+} else if(x<0){
+  print("X is a negative number")
+} else{
+  print("X equals to zero")
+}
+
