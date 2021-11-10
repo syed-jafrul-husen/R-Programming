b560469afb5bdb9191b1802acab57ae7893f2d44
Syed Jafrul Husen <syedjafrul4@gmail.com>
2021-11-10 19:35:56 +0600

    Basic graphics theory added

---
+++ DataCamp tutorial/Basic_Graphics.R
@@ -0,0 +1,28 @@
+if(FALSE){
+  "
+  Graphics in R
+      Create plots with code
+      Replication and modification easy
+      Reproducibility
+      graphics package
+      ggplot2, ggvis, lattice
+      
+      
+  Graphics package
+      Many functions
+      plot() and hist()
+        plot():
+            Generic
+            Different inputs -> Different plots
+            Vectors, Linear models, kernel desities...
+        hist():
+            Short for histogram
+            Visual representation of ditribution
+            Bin all values
+            Plot frequency of bins
+        Others graphics function
+            barplot()
+            boxplot()
+            pairs()
+  "
+}
