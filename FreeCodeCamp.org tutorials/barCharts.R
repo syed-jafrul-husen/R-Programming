34917c8639e4614512014fcf728e44d39d560861
Syed Jafrul Husen <syedjafrul4@gmail.com>
2021-11-11 20:44:50 +0600

    Bar Charts code added

---
+++ FreeCodeCamp.org tutorials/barCharts.R
@@ -0,0 +1,19 @@
+# Load datasets packages #################################
+library(datasets)
+
+
+# Load Data #############################
+?mtcars
+head(mtcars)
+
+
+# Bar charts  ###################
+barplot(mtcars$cyl)      #Doesn't work
+
+#Need a table with frequencies for each category
+cylinders <- table(mtcars$cyl)  #create table
+barplot(cylinders)    #bar chart
+plot(cylinders)     # Default X-Y plot (lines)
+
+
+# Clear environment
